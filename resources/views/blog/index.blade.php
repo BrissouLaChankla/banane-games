3064c8893b36096f0e61d9c8f4b0f20d6b8083dd
Franck.Tolosano <f.tolosano@nanaba.tech>
2022-06-10 17:24:16 +0200

    Creation de la page actualité/blog

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,8 +1,244 @@
 @extends('layouts.app')
 @section('content')
 
-Blog
-@section('prefooter')
-    @include('layouts.prefooter')
-@endsection
+<div class="container">
+    <h1 class="text-center mt-5 text-primary fs-1">A la une</h1>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card rounded-3 mt-4 border-0 shadow">
+
+                <img src="https://static.skillshare.com/cdn-cgi/image/quality=85,width=1242,height=700,format=auto/uploads/project/74915/cover_1242_da42e6ea20291d2dbdaee473756cf2d5.png" class="card-img-top object-fit" alt="" style="height:200px ;">
+
+                
+                <div class="card-body">
+
+                <div class="d-flex justify-content-start">
+                    <span class="material-symbols-rounded">calendar_today</span>
+                    <p class="card-text text-muted ms-2">  21/01/2022 </p>
+                </div>
+                    <h5 class="card-title text-primary mt-3">
+                        lorem lorem lorem lorem
+                    </h5>
+
+                    <p class="card-text my-4">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem similique assumenda non quidem nesciunt optio, praesentium ipsam nulla, atque excepturi qui quasi et a ab eos mollitia sunt facere deleniti.
+                    </p>
+
+                    <a href="#" class="text-primary text-decoration-underline fs-5"  >
+                        Lire la suite
+                    </a>
+
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="col-md-6 ">
+            <div class="card rounded-3  mt-4 border-0 shadow mb-5">
+
+                <img src="https://static.skillshare.com/cdn-cgi/image/quality=85,width=1242,height=700,format=auto/uploads/project/74915/cover_1242_da42e6ea20291d2dbdaee473756cf2d5.png" class="card-img-top object-fit" alt="" style="height:200px ;">
+
+
+                <div class="card-body">
+
+                    <div class="d-flex justify-content-start">
+                        <span class="material-symbols-rounded">calendar_today</span>
+                        <p class="card-text text-muted ms-2">  21/01/2022 </p>
+                    </div>
+                        <h5 class="card-title text-primary mt-3">
+                            lorem lorem lorem lorem
+                        </h5>
+
+                        <p class="card-text my-4">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem similique assumenda non quidem nesciunt optio, praesentium ipsam nulla, atque excepturi qui quasi et a ab eos mollitia sunt facere deleniti.
+                        </p>
+
+                        <a href="#" class="text-primary text-decoration-underline fs-5"  >
+                            Lire la suite
+                        </a>
+
+                </div>
+
+            </div>
+        </div>
+
+    <h1 class="text-center mt-5 text-primary">Les derniers articles</h1>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card rounded-3 mt-4 border-0 shadow ">
+
+                <img src="https://static.skillshare.com/cdn-cgi/image/quality=85,width=1242,height=700,format=auto/uploads/project/74915/cover_1242_da42e6ea20291d2dbdaee473756cf2d5.png" class="card-img-top object-fit" alt="" style="height:200px ;">
+
+
+                <div class="card-body">
+
+                <div class="d-flex justify-content-start">
+                    <span class="material-symbols-rounded">calendar_today</span>
+                    <p class="card-text text-muted ms-2">  21/01/2022 </p>
+                </div>
+                    <h5 class="card-title text-primary mt-3">
+                        lorem lorem lorem lorem
+                    </h5>
+
+                    <p class="card-text my-4">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem similique assumenda non quidem nesciunt optio, praesentium ipsam nulla, atque excepturi qui quasi et a ab eos mollitia sunt facere deleniti.
+                    </p>
+
+                    <a href="#" class="text-primary text-decoration-underline fs-5"  >
+                        Lire la suite
+                    </a>
+
+                </div>  
+
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card rounded-3 mt-4 border-0 shadow">
+
+                <img src="https://static.skillshare.com/cdn-cgi/image/quality=85,width=1242,height=700,format=auto/uploads/project/74915/cover_1242_da42e6ea20291d2dbdaee473756cf2d5.png" class="card-img-top object-fit" alt="" style="height:200px ;">
+
+
+                <div class="card-body">
+
+                <div class="d-flex justify-content-start">
+                    <span class="material-symbols-rounded">calendar_today</span>
+                    <p class="card-text text-muted ms-2">  21/01/2022 </p>
+                </div>
+                    <h5 class="card-title text-primary mt-3 fs-5">
+                        lorem lorem lorem lorem
+                    </h5>
+
+                    <p class="card-text my-4">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem similique assumenda non quidem nesciunt optio, praesentium ipsam nulla, atque excepturi qui quasi et a ab eos mollitia sunt facere deleniti.
+                    </p>
+
+                    <a href="#" class="text-primary text-decoration-underline fs-5"  >
+                        Lire la suite
+                    </a>
+
+                </div>  
+
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card rounded-3 mt-4 border-0 shadow">
+
+                <img src="https://static.skillshare.com/cdn-cgi/image/quality=85,width=1242,height=700,format=auto/uploads/project/74915/cover_1242_da42e6ea20291d2dbdaee473756cf2d5.png" class="card-img-top object-fit" alt="" style="height:200px ;">
+
+
+                <div class="card-body">
+
+                <div class="d-flex justify-content-start">
+                    <span class="material-symbols-rounded">calendar_today</span>
+                    <p class="card-text text-muted ms-2">  21/01/2022 </p>
+                </div>
+                    <h5 class="card-title text-primary mt-3 ">
+                        lorem lorem lorem lorem
+                    </h5>
+
+                    <p class="card-text my-4">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem similique assumenda non quidem nesciunt optio, praesentium ipsam nulla, atque excepturi qui quasi et a ab eos mollitia sunt facere deleniti.
+                    </p>
+
+                    <a href="#" class="text-primary text-decoration-underline fs-5"  >
+                        Lire la suite
+                    </a>
+
+                </div>  
+
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card rounded-3 mt-4 border-0 shadow">
+
+                <img src="https://static.skillshare.com/cdn-cgi/image/quality=85,width=1242,height=700,format=auto/uploads/project/74915/cover_1242_da42e6ea20291d2dbdaee473756cf2d5.png" class="card-img-top object-fit" alt="" style="height:200px ;">
+
+
+                <div class="card-body">
+
+                <div class="d-flex justify-content-start">
+                    <span class="material-symbols-rounded">calendar_today</span>
+                    <p class="card-text text-muted ms-2">  21/01/2022 </p>
+                </div>
+                    <h5 class="card-title text-primary mt-3">
+                        lorem lorem lorem lorem
+                    </h5>
+
+                    <p class="card-text my-4">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem similique assumenda non quidem nesciunt optio, praesentium ipsam nulla, atque excepturi qui quasi et a ab eos mollitia sunt facere deleniti.
+                    </p>
+
+                    <a href="#" class="text-primary text-decoration-underline fs-5"  >
+                        Lire la suite
+                    </a>
+
+                </div>  
+
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card rounded-3 mt-4 border-0 shadow">
+
+                <img src="https://static.skillshare.com/cdn-cgi/image/quality=85,width=1242,height=700,format=auto/uploads/project/74915/cover_1242_da42e6ea20291d2dbdaee473756cf2d5.png" class="card-img-top object-fit" alt="" style="height:200px ;">
+
+
+                <div class="card-body">
+
+                <div class="d-flex justify-content-start">
+                    <span class="material-symbols-rounded">calendar_today</span>
+                    <p class="card-text text-muted ms-2">  21/01/2022 </p>
+                </div>
+                    <h5 class="card-title text-primary mt-3">
+                        lorem lorem lorem lorem
+                    </h5>
+
+                    <p class="card-text my-4">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem similique assumenda non quidem nesciunt optio, praesentium ipsam nulla, atque excepturi qui quasi et a ab eos mollitia sunt facere deleniti.
+                    </p>
+
+                    <a href="#" class="text-primary text-decoration-underline fs-5"  >
+                        Lire la suite
+                    </a>
+
+                </div>  
+
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card rounded-3 mt-4 border-0 shadow ">
+
+                <img src="https://static.skillshare.com/cdn-cgi/image/quality=85,width=1242,height=700,format=auto/uploads/project/74915/cover_1242_da42e6ea20291d2dbdaee473756cf2d5.png" class="card-img-top object-fit" alt="" style="height:200px ;">
+
+
+                <div class="card-body">
+
+                <div class="d-flex justify-content-start">
+                    <span class="material-symbols-rounded">calendar_today</span>
+                    <p class="card-text text-muted ms-2">  21/01/2022 </p>
+                </div>
+                    <h5 class="card-title text-primary mt-3">
+                        lorem lorem lorem lorem
+                    </h5>
+
+                    <p class="card-text my-4">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem similique assumenda non quidem nesciunt optio, praesentium ipsam nulla, atque excepturi qui quasi et a ab eos mollitia sunt facere deleniti.
+                    </p>
+
+                    <a href="#" class="text-primary text-decoration-underline fs-5"  >
+                        Lire la suite
+                    </a>
+
+                </div>  
+
+            </div>
+        </div>
+  
+    </div>
+
+</div>
+
+<div class="container my-5"></div>
+
 @endsection
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,81 @@
 
 
 <footer>
-    <a href="#">Nanagames</a>
-    <a href="#">Nanagames</a>
-    <a href="#">Nanagames</a>
-    <a href="#">Nanagames</a>
-    <a href="#">Nanagames</a>
-    <a href="#">Nanagames</a>
+
+
+    <div class="container-lg">
+        <h1 class="mt-4 fs-1 text-center text-md-start"> Nanagames</h1>
+        <div class="row">
+            <div class="col-md-3">
+                <div class=" my-3 text-center text-md-start">
+                    <h5 class="my-3 text-white fs-3">Découvrez-nous</h5>
+                    <a href="#" class="fs-4">Nanaba</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class=" my-3 text-center text-md-start">
+                    <h5 class="my-3 text-white fs-3">Contact</h5>
+                    <a href="#" class="fs-4">Nous contacter</a><br>
+                    <a href="#" class="fs-4">Nous rejoindre</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <h5 class="my-3 text-white fs-3 text-center text-md-start">Suivez nous</h5>
+                <div class=" text-center text-md-start">
+                        <a href="https://www.facebook.com/nanabaofficiel/" class="me-2"><img src="{{ asset('img/footer/akar-icons-facebook-fill.svg') }}" alt="Icon facebook"></a>
+                        <a href="https://www.instagram.com/nanabaofficiel/?hl=fr" class="me-2"><img src="{{ asset('img/footer/akar-icons-instagram-fill.svg') }}" alt="Icon Instagram"></a>
+                        <a href="https://twitter.com/nanabaofficiel" class="me-2"><img src="{{ asset('img/footer/akar-icons-twitter-fill.svg') }}" alt="Icon Twitter"></a>
+                        <a href="https://www.linkedin.com/company/nanabaofficiel/mycompany/" class="me-2"><img src="{{ asset('img/footer/akar-icons-linkedin-fill.svg') }}" alt="Icon Linkdin"></a>
+                        <a href="https://www.tiktok.com/@misternanaba" class="me-2"><img src="{{ asset('img/footer/bi-tiktok.svg') }}" alt="Icon Tiktok"></a>
+                </div>
+
+            </div>
+
+            <div class="col-md-3">
+                <div class="my-3 text-center text-md-start ">
+                    <h5 class="my-3 text-white fs-3">S’inscrire à la newsletter</h5>
+                    <form>
+                        <div class="mb-3">
+                            <div class="d-flex align-items-start">
+                                <!-- <input type="email" class="form-control" id="" aria-describedby="" placeholder="Votre email">
+                                <button type="submit" class="btn-primary">OK</button> -->
+
+                                <form method="POST" action="http://nanaba-nanagames.test/add-email-newsletter" accept-charset="UTF-8" class="d-flex">
+                                    <input name="_token" type="hidden" value="oMybaaYpBHSHxjO8CulGFferNRJTJkGORGhvrgq7">
+                                    <div class="flex-grow-1">
+                                        <input class="form-control text-muted border-0 rounded" placeholder="Votre email" required="" name="email" type="email">
+                                    </div>
+                                    <div>
+                                        <button class=" btn btn-white ms-2 p-1">OK</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row justify-content-md-center text-center">
+
+
+            <div class="col-md-4">
+                <h5><a href="#" class="fs-4 fw-bold">Mentions légales</a></h5>
+            </div>
+
+            <div class="col-md-4">
+               <h5><a href="#" class="fs-4 fw-bold"> Chartes des cookies</a></h5>
+            </div>
+
+            <div class="col-md-4">
+                 <h5><a href="#" class="fs-4 fw-bold"> CGV / CGU</a></h5>
+            </div>
+
+        </div>
+
+
+    </div>
 </footer>
